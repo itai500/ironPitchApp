836d9075fbfcd70341e84b1fb1d8728b00184e6c
Michael Iline <michael.iline@ironsrc.com>
2017-11-30 15:05:42 +0200

    feat(AR section):

--- AppStoreiOS11-master/AppStore iOS 10/TodaysAppsController.swift
+++ AppStoreiOS11-master/AppStore iOS 10/TodaysAppsController.swift
@@ -60,7 +60,20 @@ class TodaysAppsController: DatasourceController  {
         
         let app3 = App(appName: "Custom Offerwall", appDesc: "Holidays Special", appID: 3, appCategory: "Offerwall", appPrice: 0, appStory:"123", appPhoto: "OfferWall_custom", screenShots: [""], appWebViewDemo:"https://supersonicads-a.akamaihd.net/adunit/dist/prod/adUnitStandalone.html?localTime=1512025409633&format=openRTB&bundleId=com.ironsrcmobile.UltraApp&batteryLevel=-100&deviceVolume=0&deviceModel=iPad4%2C4&scope=8&applicationKey=45e3185d&deviceIds%5BIFA%5D=09188F19-78BB-443A-BB76-3D600BAB62CE&appVersion=6.7.3&deviceLanguage=EN&mnc=0&gpi=false&country=&referrerId=mobileApps2Go&controllerId=1512025407472_0.33921600014210296&applicationUserId=ggggg&debug=3&pScore=undefined&deviceOs=ios&v=2.2.10&appOrientation=none&deviceScreenScale=2&deviceOEM=Apple&iphoneAppOnIpadDevice=0&jb=false&diskFreeSize=760&timezoneOffset=-120&baseUrl=https%3A%2F%2Fow-gateway.supersonicads.com%2Fgateway%2Fofferwall%2Frequest&allowArbitraryLoads=true&mcc=0&suppressPageImpression=1&referrerBanners=20&deviceWidthDP=1024&deviceOSVersion=11.0.3&deviceHeightDP=768&isLimitAdTrackingEnabled=false&sdkAbName=7.1&SDKVersion=5.47&connectionType=wifi&placementId=0", appVideoDemo: "https://content.jwplatform.com/videos/kaUXWqTZ-640.mp4")
         
-        apps = [app1, app2, app3]
+        let arrSection = App(appName: "AR Video",
+                             appDesc: "Augmented reality advertising",
+                             appID: 3,
+                             appCategory: "AR Video",
+                             appPrice: 0,
+                             appStory:"123",
+                             appPhoto: "ar_custom",
+                             screenShots: [""],
+                             appWebViewDemo:"",
+                             appVideoDemo: "https://ssastatic.s3.amazonaws.com/test/hackaton2017/pitchapp/CSR_Racing-IronSourceAR.mp4"
+        )
+        
+        apps = [app1, app2, app3, arrSection]
+        
         let headerItemGames = HeaderItem(smallTitle: "\t".uppercased(), bigTitle: "Product Demos", hideDiv: false)
         
         var configGames = ViewControllerConfigurator()
